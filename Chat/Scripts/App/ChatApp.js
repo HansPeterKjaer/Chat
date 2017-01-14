88d34eaec72af5a8457ed8b0d64e879f2f49fffb
HP <hpkjaer@protonmail.com>
2017-01-14 22:56:52 +0100

    basic angular app + markup

---
+++ Chat/Scripts/App/ChatApp.js
@@ -0,0 +1,29 @@
+﻿app = angular.module('ChatApp', []);
+app.controller('ChatController', function ($scope, $http) {
+    $scope.name = null;
+    $scope.messages = [];
+
+    $scope.register = function (name) {
+        console.log($scope._name);
+        $http({
+            method: 'POST',
+            url: '/api/ChatUsers',
+            data: { 'name': name }
+        }).success(function (data, status, headers, config) {
+            $scope.name = data.name;
+            $scope.id = data.id;
+        }).error(function (data, status, headers, config) {
+            // Error handling not implemented yet.
+        });
+    };
+
+    $scope.submitMessage = function (msg) {
+        $http({
+            method: 'POST',
+            url: 'api/ChatMessages/',
+            data: { 'ChatUserId': $scope.id, 'MessageBody': msg }
+        }).success(function (data, status, headers, config) {
+            $scope.messages.push(data);
+        });
+    };
+});
